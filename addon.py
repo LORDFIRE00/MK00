03880c9db1da3d581e75712ef332f0c3d4d7466a
LORDFIRE00 <lordfire00@gmail.com>
2015-10-07 23:41:27 -0500

    Update addon.py

--- addon.py
+++ addon.py
@@ -5,7 +5,7 @@ __addon__       = xbmcaddon.Addon()
 __addonname__   = __addon__.getAddonInfo('name')
  
 line1 = "Hello World!"
-line2 = "We can write anything we want here"
+line2 = "this is a test"
 line3 = "Using Python"
  
 xbmcgui.Dialog().ok(__addonname__, line1, line2, line3)
