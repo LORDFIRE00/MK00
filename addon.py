9e144c30913a6b7f721c3dcbcb4cab95f72574ed
LORDFIRE00 <lordfire00@gmail.com>
2015-10-08 00:12:40 -0500

    Update addon.py

--- addon.py
+++ addon.py
@@ -4,8 +4,8 @@ import xbmcgui
 __addon__       = xbmcaddon.Addon()
 __addonname__   = __addon__.getAddonInfo('name')
  
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
+line1 = "goodbye says mediakings World!"
+line2 = "test window popup"
 line3 = "Using Python"
  
 xbmcgui.Dialog().ok(__addonname__, line1, line2, line3)
